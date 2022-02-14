3752753df83dd39668161e4e09abe03ab4359204
JerryCarson <redstonemaster20@gmail.com>
2022-02-14 16:55:28 +0300

    nothing

--- gui.py
+++ gui.py
@@ -1,5 +1,4 @@
-import ser
-import random
+import port
 import numpy as np
 from appJar import gui
 
@@ -17,17 +16,19 @@ app.setEntry("f1", "C://Users//1//Desktop/s.txt")
 
 
 def getXY():
-    y=[]
+    y = []
     with open("output.txt", "r") as input:
-                for line in input:
-                    y.append(float(line))
+        for line in input:
+            y.append(float(line))
     x = np.arange(0.0, np.size(y), 1)
-    return x,y 
+    return x, y
+
 
 def showLabels():
     axes.set_xlabel("Time")
     axes.set_ylabel("Amplitude")
     app.refreshPlot("p1")
+
 
 def press(button):
     if button == "Cancel":
@@ -37,13 +38,11 @@ def press(button):
             with open("input.txt", "w") as input:
                 for line in app.getTextArea("t1"):
                     input.write(line)
-            c = ser.SerialWrite(
-                app.getEntry("COM port name"),
-                app.getEntry("Baud rate"),
-                "input.txt",
+            c = port.SerialWrite(
+                app.getEntry("COM port name"), app.getEntry("Baud rate"), "input.txt"
             )
         else:
-            c = ser.SerialWrite(
+            c = port.SerialWrite(
                 app.getEntry("COM port name"),
                 app.getEntry("Baud rate"),
                 app.getEntry("f1"),
@@ -53,8 +52,8 @@ def press(button):
         axes = app.updatePlot("p1", *getXY())
         showLabels()
 
+
 app.addButtons(["Submit", "Cancel"], press)
 axes = app.addPlot("p1", *getXY())
 showLabels()
-
 app.go()
