b9e2c390191096a7669982b214cada093f756132
JerryCarson <redstonemaster20@gmail.com>
2022-02-14 13:30:55 +0300

    minor upd

--- 1.py
+++ 1.py
@@ -7,12 +7,11 @@ serial_port = "COM19"  # set your COM port value
 baud_rate = 9600
 # set same as Arduino has
 read_file_path = "input.txt"
-write_file_path = "output.txt"
 
 with serial.Serial(serial_port, baud_rate, timeout = 0) as ardu: #opens and automatically closes COM port after use
-    with open(read_file_path, "r") as input_file, open(write_file_path, "w") as output_file:
+    with open(read_file_path, "r") as input_file:
         for line in input_file:
-            ardu.write(line)
+            ardu.write(line.strip())
 
 # data = np.loadtxt('output.txt')
 # y = data[:]
