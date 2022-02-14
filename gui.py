55fe522048ed3da24419ec4d7e662b0672978081
JerryCarson <redstonemaster20@gmail.com>
2022-02-14 15:38:10 +0300

    gui update

--- gui.py
+++ gui.py
@@ -2,17 +2,22 @@ import ser
 from appJar import gui
 
 app = gui()
+app.addRadioButton("ft", "Use file")
+app.addRadioButton("ft", "Use text")
 app.addFileEntry("f1")
 app.addTextArea("t1")
 app.addLabelEntry("COM port name")
 app.addLabelEntry("Baud rate")
+app.setEntry("COM port name", "COM1")
+app.setEntry("Baud rate", 9600)
+app.setEntry("f1", "C://Users//1//Desktop/s.txt")
 
 
 def press(button):
     if button == "Cancel":
         app.stop()
     else:
-        if app.getTextArea("t1") != "":
+        if app.getRadioButton("ft") == "Use text":
             with open("input.txt", "w") as input:
                 for line in app.getTextArea("t1"):
                     input.write(line)
@@ -29,6 +34,7 @@ def press(button):
                 app.getEntry("f1"),
             )
             c.comWrite()
+
 
 app.addButtons(["Submit", "Cancel"], press)
 
