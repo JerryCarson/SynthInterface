52114945c3509e13a72d732c0fed5493eadba572
JerryCarson <redstonemaster20@gmail.com>
2022-02-14 16:35:08 +0300

    test gui with plot

--- gui.py
+++ gui.py
@@ -1,5 +1,8 @@
 import ser
+import random
+import numpy as np
 from appJar import gui
+
 
 app = gui()
 app.addRadioButton("ft", "Use file")
@@ -12,6 +15,19 @@ app.setEntry("COM port name", "COM1")
 app.setEntry("Baud rate", 9600)
 app.setEntry("f1", "C://Users//1//Desktop/s.txt")
 
+
+def getXY():
+    y=[]
+    with open("output.txt", "r") as input:
+                for line in input:
+                    y.append(float(line))
+    x = np.arange(0.0, np.size(y), 1)
+    return x,y 
+
+def showLabels():
+    axes.set_xlabel("Time")
+    axes.set_ylabel("Amplitude")
+    app.refreshPlot("p1")
 
 def press(button):
     if button == "Cancel":
@@ -26,16 +42,19 @@ def press(button):
                 app.getEntry("Baud rate"),
                 "input.txt",
             )
-            c.comWrite()
         else:
             c = ser.SerialWrite(
                 app.getEntry("COM port name"),
                 app.getEntry("Baud rate"),
                 app.getEntry("f1"),
             )
-            c.comWrite()
-
+        c.comWrite()
+        app.refreshPlot("p1")
+        axes = app.updatePlot("p1", *getXY())
+        showLabels()
 
 app.addButtons(["Submit", "Cancel"], press)
+axes = app.addPlot("p1", *getXY())
+showLabels()
 
 app.go()
